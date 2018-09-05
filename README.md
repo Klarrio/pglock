4a3f0ad5a47f45ea9c84f38272a68866c406a840
Timo Beckers <timo@incline.eu>
2018-09-05 11:02:33 +0200

    Add --wait flag to block until lock is available

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # `pglock`
 
-Use PostgreSQL advisory locking to run a command exactly once over many
+Use PostgreSQL advisory locking to run commands safely and serially over many
 concurrent invocations. Inspired by `consul lock`.
 
 ---
@@ -8,11 +8,67 @@ concurrent invocations. Inspired by `consul lock`.
 ## Usage
 
 ```
-export PGLOCK_PASS="secret-password"
-pglock --user postgres sleep 2
+~ pglock --help
+
+pglock runs a command while holding a lock in a PostgreSQL database.
+Any concurrent invocations that fail to obtain a lock will not run
+the given command and will exit with return code 0.
+
+The flags below can be configured as environment variables with the PGLOCK_ prefix.
+PGLOCK_PASS needs to be configured to authenticate to the database.
+
+      --database string   Database name to connect to on PostgreSQL. (default "postgres")
+      --host string       Hostname of the PostgreSQL instance. (default "localhost")
+      --lockid uint32     The numeric lock ID to claim in PostgreSQL. (default 1)
+      --port uint16       Port the PostgreSQL instance is listening on. (default 5432)
+      --sslmode string    The SSL mode of the PostgreSQL client. (default "disable")
+      --user string       Username to authenticate to the PostgreSQL.
+      --wait uint32       Amount of seconds to wait for a lock to be obtained.
 ```
 
-When executed on many hosts concurrently, only the first agent
-that manages to obtain a lock will execute, the others will exit.
+### Non-blocking (fall-through)
 
-Does not (yet) support retries or waiting for other processes to terminate.
+```
+export PGLOCK_PASS="secret-password"
+pglock --user postgres sleep 2
+2018/09/05 11:17:39 Connecting to PostgreSQL at postgres@localhost:5432/postgres.
+2018/09/05 11:17:39 Trying to obtain lock.
+2018/09/05 11:17:40 Lock ID 1 obtained successfully!
+2018/09/05 11:17:40 Executing command: [sleep 2]
+2018/09/05 11:17:42 Execution finished, exiting and cleaning up lock.
+
+```
+
+When executed on many hosts concurrently, only the first agent that manages to
+obtain a lock will execute, the others will exit. This does not ensure that
+all agents will run their commands; see [Blocking](#Blocking) for this use case.
+
+### Blocking
+
+Blocking mode waits for the lock to be obtained, up to a given timeout
+(in seconds). This will guarantee that all agents execute their commands,
+as long as each of them can obtain the lock within the timeout.
+
+```
+export PGLOCK_PASS="secret-password"
+pglock --user postgres --wait 10 sleep 2
+2018/09/05 11:22:14 Connecting to PostgreSQL at postgres@localhost:5432/postgres.
+2018/09/05 11:22:14 Obtaining lock with 10 seconds timeout..
+2018/09/05 11:22:14 Lock ID 1 obtained successfully!
+2018/09/05 11:22:14 Executing command: [sleep 2]
+2018/09/05 11:22:16 Execution finished, exiting and cleaning up lock.
+```
+
+When running multiple concurrent instances in blocking mode, the following
+output would be expected:
+
+```
+export PGLOCK_PASS="secret-password"
+pglock --user postgres --wait 2 sleep 10
+2018/09/05 11:27:50 Connecting to PostgreSQL at postgres@localhost:5432/postgres.
+2018/09/05 11:27:50 Obtaining lock with 2 seconds timeout..
+2018/09/05 11:27:53 Could not obtain lock 1, skipping command
+```
+
+The given `sleep 10` command will not be executed, and the process will return
+with return code 0.
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"time"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -21,6 +23,7 @@ const (
 	flagDatabase = "database"
 	flagLockID   = "lockid"
 	flagSSLMode  = "sslmode"
+	flagWait     = "wait"
 )
 
 func main() {
@@ -32,7 +35,8 @@ func main() {
 	port := viper.GetInt(flagPort)
 	dbName := viper.GetString(flagDatabase)
 	sslMode := viper.GetString(flagSSLMode)
-	lockID := viper.GetInt(flagLockID)
+	wait := uint32(viper.GetInt(flagWait))
+	lockID := uint32(viper.GetInt(flagLockID))
 
 	// List of positional arguments after all flags have been parsed.
 	cmd := pflag.Args()
@@ -52,7 +56,15 @@ func main() {
 	defer db.Close()
 
 	// Take out lock from database
-	lock, err := getLock(db, uint32(lockID))
+	var lock bool
+	if wait == 0 {
+		log.Printf("Trying to obtain lock.")
+		lock, err = getLockTry(db, lockID)
+	} else {
+		log.Printf("Obtaining lock with %d seconds timeout..", wait)
+		lock, err = getLockWait(db, lockID, wait)
+	}
+
 	if err != nil {
 		log.Fatalf("Error trying to obtain lock: %s", err.Error())
 	}
@@ -76,9 +88,9 @@ func main() {
 	os.Exit(0)
 }
 
-// getLock attempts to take out a lock from the database. Does not block.
+// getLockTry attempts to take out a lock from the database. Always immediately returns.
 // Returns true if the lock was successfully obtained, false when the lock is held by another client.
-func getLock(db *sql.DB, lockID uint32) (bool, error) {
+func getLockTry(db *sql.DB, lockID uint32) (bool, error) {
 
 	var lock bool
 	err := db.QueryRow("SELECT pg_try_advisory_lock($1);", lockID).Scan(&lock)
@@ -87,6 +99,30 @@ func getLock(db *sql.DB, lockID uint32) (bool, error) {
 	}
 
 	return lock, nil
+}
+
+// getLockWait attempts to take out a lock from the database, blocking until the lock is available.
+// Returns true if the lock was successfully obtained, false when the lock is (still) held by another
+// client at the time of the expiry of the wait timer.
+func getLockWait(db *sql.DB, lockID uint32, wait uint32) (bool, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(wait)*time.Second)
+	defer cancel()
+
+	// pg_advisory_lock does not return any values. If the query returns,
+	// the lock was obtained.
+	_, err := db.QueryContext(ctx, "SELECT pg_advisory_lock($1);", lockID)
+	if err, ok := err.(*pq.Error); ok {
+		// If the query was cancelled, return false (no lock obtained)
+		// 57014 is the postgres error code for canceled queries
+		if err.Code == "57014" {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+
+	return true, nil
 }
 
 func init() {
@@ -109,12 +145,13 @@ PGLOCK_PASS needs to be configured to authenticate to the database.
 	viper.SetEnvPrefix("pglock")
 
 	// Command line flags
-	pflag.Uint32("lockid", 1, "The numeric lock ID to claim in PostgreSQL.")
-	pflag.String("host", "localhost", "Hostname of the PostgreSQL instance.")
-	pflag.Uint16("port", 5432, "Port the PostgreSQL instance is listening on.")
-	pflag.String("database", "postgres", "Database name to connect to on PostgreSQL.")
-	pflag.String("user", "", "Username to authenticate to the PostgreSQL.")
-	pflag.String("sslmode", "disable", "The SSL mode of the PostgreSQL client.")
+	pflag.Uint32(flagLockID, 1, "The numeric lock ID to claim in PostgreSQL.")
+	pflag.String(flagHost, "localhost", "Hostname of the PostgreSQL instance.")
+	pflag.Uint16(flagPort, 5432, "Port the PostgreSQL instance is listening on.")
+	pflag.String(flagDatabase, "postgres", "Database name to connect to on PostgreSQL.")
+	pflag.String(flagUser, "", "Username to authenticate to the PostgreSQL.")
+	pflag.String(flagSSLMode, "disable", "The SSL mode of the PostgreSQL client.")
+	pflag.Uint32(flagWait, 0, "Amount of seconds to wait for a lock to be obtained.")
 
 	pflag.Parse()
 
